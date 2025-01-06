d36445993c7c387e8a6015a61a7419eed9915a95
kamini <kaminigohatre05255@gmail.com>
2025-01-06 12:55:58 +0530

    updated

--- day-3-statefile/backend.tf
+++
@@ -1,7 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket = "5jan"
-    key    = "terraform.tfstate"
-    region = "ap-south-1"
-  }
-}
---
+++ day-4-statefile/main.tf
@@ -0,0 +1,11 @@
+resource "aws_instance" "dev" {
+  ami               = "ami-0fd05997b4dff7aac"
+  instance_type     = "t2.micro"
+  key_name          = "16DEC"
+  availability_zone = "ap-south-1a"
+  tags = {
+    Name = "dev"
+  }
+
+
+}
